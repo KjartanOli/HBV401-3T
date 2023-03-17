13ff0f46a8e31cceb7836b1d68902e9670303ae7
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-03-17 10:48:33 +0000

    Initial commit

---
+++ src/main/java/is/hi/travel_planer/SystemInfo.java
@@ -0,0 +1,13 @@
+package is.hi.travel_planer;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module is.hi.travel_planer {
+    requires javafx.controls;
+    exports is.hi.travel_planer;
+}
