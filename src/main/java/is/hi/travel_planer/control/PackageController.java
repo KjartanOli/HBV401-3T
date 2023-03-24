f68561668d26795efa593fb93fb8119db340665a
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-03-24 18:40:14 +0000

    Stop storing return values of other groups' APIs
    * src/main/java/is/hi/travel_planer/control/PackageController.java (PackageController):
    Add getFlights, getHotels, getTours
    Remove instance variables flights, hotels, tours

--- src/main/java/is/hi/travel_planer/control/PackageController.java
+++ src/main/java/is/hi/travel_planer/control/PackageController.java
@@ -2,6 +2,7 @@ package is.hi.travel_planer.control;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import is.hi.travel_planer.model.TravelPackage;
 import is.hi.travel_planer.model.User;
@@ -24,24 +25,30 @@ public class PackageController {
 	private DayTourDetails[] tours;
 	private Hotel[] hotels;
 
-	public PackageController(User user, FlightInterface flightController, IHotelController hotelController, DayTourController dayTourController) {
+	private List<Flight> getFlights() {
+		return this.flightController.searchFlights(user.getLocation(), user.getDestination(), user.getDepartureDate());
+	}
+
+	private List<Hotel> getHotels() {
+		return this.hotelController.searchHotels(user.getDepartureDate(), user.getGroupSize(), 0, user.getDestination());
+	}
+
+	private List<DayTourDetails> getTours() {
+		return Arrays.asList(dayTourController.searchTours(user.getDestination(), user.getDepartureDate(), user.getReturnDate()));
+	}
+
+	public PackageController(User user, FlightControllerInterface flightController, IHotelController hotelController, DayTourController dayTourController) {
 		this.user = user;
+		this.flightController = flightController;
 		this.hotelController = hotelController;
 		this.dayTourController = dayTourController;
-		this.flights = flightController.searchFlight(user.getLocation(), user.getDestination(), user.getDepartureDate());
-		this.hotels = this.hotelController.searchHotels(user.getDepartureDate(), user.getGroupSize(), 0, user.getDestination()).toArray(new Hotel[0]);
-		var t = dayTourController.searchTours(user.getDestination(), user.getDepartureDate(), user.getReturnDate());
-		this.tours = new DayTourDetails[t.length];
-		for (int i = 0; i < t.length; ++i) {
-			this.tours[i] = (DayTourDetails) t[i];
-		}
 	}
 
 	public List<TravelPackage> createPackages(Flight flight) {
-		List<TravelPackage> packages = new ArrayList<TravelPackage>(this.hotels.length * this.tours.length);
+		List<TravelPackage> packages = new ArrayList<TravelPackage>();
 
-		for (var hotel : this.hotels) {
-			for (var tour : this.tours) {
+		for (var hotel : this.getHotels()) {
+			for (var tour : this.getTours()) {
 				var t = new TravelPackage(flight, hotel, tour, user.getTripDuration());
 				packages.add(t);
 			}
@@ -51,10 +58,10 @@ public class PackageController {
 	}
 
 	public List<TravelPackage> createPackages(Hotel hotel) {
-		List<TravelPackage> packages = new ArrayList<TravelPackage>(this.flights.length * this.tours.length);
+		List<TravelPackage> packages = new ArrayList<TravelPackage>();
 
-		for (var flight : this.flights) {
-			for (var tour : this.tours) {
+		for (var flight : this.getFlights()) {
+			for (var tour : this.getTours()) {
 				var t = new TravelPackage(flight, hotel, tour, user.getTripDuration());
 				packages.add(t);
 			}
@@ -68,10 +75,10 @@ public class PackageController {
 	@return The list of all packages that include the given tour
 	 */
 	public List<TravelPackage> createPackages(DayTourDetails tour) {
-		List<TravelPackage> packages = new ArrayList<TravelPackage>(this.flights.length * this.hotels.length);
+		List<TravelPackage> packages = new ArrayList<TravelPackage>();
 
-		for (var flight : this.flights) {
-			for (var hotel : this.hotels) {
+		for (var flight : this.getFlights()) {
+			for (var hotel : this.getHotels()) {
 				var t = new TravelPackage(flight, hotel, tour, user.getTripDuration());
 				packages.add(t);
 			}
@@ -81,10 +88,10 @@ public class PackageController {
 	}
 
 	public List<TravelPackage> createPackages() {
-		List<TravelPackage> packages = new ArrayList<TravelPackage>(this.flights.length * this.hotels.length * this.hotels.length);
-		for (var flight : this.flights) {
-			for (var hotel : this.hotels) {
-				for (var tour : this.tours) {
+		List<TravelPackage> packages = new ArrayList<TravelPackage>();
+		for (var flight : this.getFlights()) {
+			for (var hotel : this.getHotels()) {
+				for (var tour : this.getTours()) {
 					var t = new TravelPackage(flight, hotel, tour, user.getTripDuration());
 					packages.add(t);
 				}
