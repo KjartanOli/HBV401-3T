a2709fc4d1f4b16d44959332746ebdfbf0c98eca
Sverrir Sigfusson <s.sigfusson@icloud.com>
2023-03-25 16:46:25 +0000

    Minor changes in PCTest

--- src/test/java/is/hi/travel_planer/control/PackageControllerTest.java
+++ src/test/java/is/hi/travel_planer/control/PackageControllerTest.java
@@ -12,6 +12,7 @@ import is.hi.hotel.interfaces.IHotelController;
 import is.hi.daytour.DayTourController;
 
 import is.hi.hotel.entities.Hotel;
+import is.hi.hotel.entities.Room;
 import is.hi.hotel.entities.Dates;
 
 public class PackageControllerTest {
@@ -21,12 +22,15 @@ public class PackageControllerTest {
 	public void setUp() {
 		var user = new User(
 			"John Doe", "john@example.org", "1234567890", "1234567", 2,
-			"Reykjavík", LocalDate.of(2023, 3, 12),
-			"Akureyri", LocalDate.of(2023, 3, 17));
+			"Reykjavík", LocalDate.of(2023, 3, 24),
+			"Akureyri", LocalDate.of(2023, 3, 28));
 		var hc = new HotelControllerMock();
 		var dc = new DayTourController();
 		var fc = new FlightControllerMock();
 		controller = new PackageController(user, fc, hc, dc);
+
+		var hotel = new Hotel(1, "Akureyri", 1, new Room(101, 4, false,
+						new Dates(LocalDate.of(2023,3,12), LocalDate.of(2023,3,27))));
 	}
 
 	@After
