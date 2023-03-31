d2de04019bf742d98f459f9c82c606fed2a208fc
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-03-31 17:39:46 +0000

    Require javafx modules, and open view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module is.hi.travel_planer {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires is.hi.flight_booking;
-    requires com.daytour;
-    exports is.hi.travel_planer;
-    exports is.hi.travel_planer.mock;
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+
+	opens is.hi.travel_planer.view to javafx.fxml;
+	requires is.hi.flight_booking;
+	requires com.daytour;
+
+	exports is.hi.travel_planer;
+	exports is.hi.travel_planer.mock;
 }
