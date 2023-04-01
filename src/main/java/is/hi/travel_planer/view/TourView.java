e31a7c35203f821df4e76f4b2d20b641ae0fd047
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-04-01 21:23:38 +0000

    Create component to display DayTours
    This component is primarily intended as a part of a larger component
    displaying TravelPackages.

---
+++ src/main/java/is/hi/travel_planer/view/TourView.java
@@ -0,0 +1,34 @@
+package is.hi.travel_planer.view;
+
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.layout.HBox;
+
+import com.daytour.processing.DayTourDetails;
+
+public class TourView extends HBox {
+	@FXML
+	private Label name;
+
+	public TourView() {
+		var loader = new FXMLLoader(getClass().getResource("/fxml/TourView.fxml"));
+		loader.setRoot(this);
+		loader.setController(this);
+		try {
+			loader.load();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public TourView(DayTourDetails tour) {
+		this();
+		this.setTour(tour);
+	}
+
+	public void setTour(DayTourDetails tour) {
+		name.setText(Long.toString(tour.getID()));
+	}
+}
