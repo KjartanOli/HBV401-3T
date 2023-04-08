2aeed32a611017122cd1017375f0dc942e5e330e
Kjartan Óli Ágústsson <kjartanoli@disroot.org>
2023-04-08 19:14:54 +0000

    Conditionally enable next button on detail selection screen
    * src/main/java/is/hi/travel_planer/view/DetailSelectionController.java:
    (selectedRoomCapacity,selectedRooms,selectedTime,enableNextButton, handleNext): Add
    * src/main/resources/fxml/DetailSelection.fxml: Add fx:id and onAction

--- src/main/java/is/hi/travel_planer/view/DetailSelectionController.java
+++ src/main/java/is/hi/travel_planer/view/DetailSelectionController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.SelectionMode;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 import is.hi.travel_planer.model.TravelPackage;
 import is.hi.travel_planer.model.User;
@@ -24,6 +28,9 @@ public class DetailSelectionController {
 	@FXML
 	private ListView<TourTime> tourTimeSelector;
 
+	@FXML
+	private Button next;
+
 	private TravelPackage pkg;
 	private User user;
 	private PackageController packageController;
@@ -36,19 +43,42 @@ public class DetailSelectionController {
 
 	@FXML
 	private void initialize() {
+		enableNextButton();
+
 		seatSelector.setFlight(pkg.getFlight());
+		seatSelector.setGroupSize(user.getGroupSize());
+		seatSelector.addEventHandler(ActionEvent.ACTION, event -> {
+				enableNextButton();
+				event.consume();
+			});
+
 		roomSelector.getItems().setAll(pkg.getHotel().getRooms());
-		roomSelector.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		roomSelector.setCellFactory(lv -> new RoomCell());
+		roomSelector.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		roomSelector.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Room>() {
+			@Override
+			public void changed(ObservableValue<? extends Room> observable, Room oldValue, Room newValue) {
+				System.err.println("test1");
+				enableNextButton();
+			}
+		});
+
 		tourTimeSelector.getItems().setAll(TourTime.from(pkg.getTour(), user.getTripDuration()));
 		tourTimeSelector.setCellFactory(lv -> new TourTimeCell());
+		tourTimeSelector.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TourTime>() {
+			@Override
+			public void changed(ObservableValue<? extends TourTime> observable, TourTime oldValue, TourTime newValue) {
+				System.err.println("test2");
+				enableNextButton();
+			}
+		});
 	}
 
 	private List<Room> selectedRooms() {
 		return roomSelector.getSelectionModel().getSelectedItems();
 	}
 
-	private TourTime selecteTime() {
+	private TourTime selectedTime() {
 		return tourTimeSelector.getSelectionModel().getSelectedItem();
 	}
 
@@ -58,5 +88,19 @@ public class DetailSelectionController {
 			c += room.getCapacity();
 
 		return c;
+	}
+
+	private void enableNextButton() {
+		if (seatSelector.getSelected().size() < user.getGroupSize()
+			|| selectedRoomCapacity() < user.getGroupSize()
+			|| selectedTime() == null)
+			next.setDisable(true);
+		else
+			next.setDisable(false);
+	}
+
+	@FXML
+	private void handleNext() {
+		System.err.println("test");
 	}
 }
